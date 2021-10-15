2d5f7bef30e31ba1196ecfeb1a7ac5c1c3fafeb6
Wesley Bruno <84283440+weskbru@users.noreply.github.com>
2021-10-14 22:06:58 -0300

    Update senha.java

--- src/senha/senha.java
+++ src/senha/senha.java
@@ -1,14 +1,18 @@
 package senha;
 
+// importando pacote que interage com usuario
 import javax.swing.JOptionPane;
 
 public class senha {
     public static void main(String[] args){
+        // desclare as variaveis 
         int senhaDeEntrada = 0;
-        int senha = 1234;
+        int senha = 1234; // opicional
         
+        // mostra uma caixa de entrada de dados iteragindo  com usuario 
         senhaDeEntrada = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a senha de 4 digitos:","login",1)); 
         
+        // metodo de comparçao de entrada de dados 
         if(senha == senhaDeEntrada){
             JOptionPane.showMessageDialog(null, "Acesso permitido");
         }else{
