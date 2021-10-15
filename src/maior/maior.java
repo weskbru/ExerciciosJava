a670c63cadbefdb43cb02b039d697ab7104583c0
Wesley Bruno <84283440+weskbru@users.noreply.github.com>
2021-10-14 22:26:53 -0300

    Update maior.java

--- src/maior/maior.java
+++ src/maior/maior.java
@@ -4,14 +4,17 @@ import javax.swing.JOptionPane;
 
 public class maior {
     public static void main(String[] args){
+        // declare as variaveis
        int n1 = 0;
        int n2 =0;
        int n3 = 0;
         
+        // faça a conversão das variaveis de String para int 
         n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite 1° numero:"));
         n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite 2° numero:"));
         n3 = Integer.parseInt(JOptionPane.showInputDialog("Digite 3° numero:"));
         
+        // realize a comparação das variaveis 
         if(n1 >= n2 && n1 >= n3){
             JOptionPane.showMessageDialog(null, "O numero maior digitado foi " +n1);
         }else if(n2 >= n1 && n2 >= n3){
